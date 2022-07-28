ef802b682ab751336b30e3aed120878a35d10649
Pavel Ushakov <ushakovpb@gmail.com>
2022-07-28 20:41:34 +0400

    Вычисление комбинациий типов данных

---
+++ src/main/java/guru/qa/homework1/Combinations.java
@@ -0,0 +1,22 @@
+package guru.qa.homework1;
+
+public class Combinations {
+    public static void main(String[] args) {
+        byte oByte = 1;
+        short oShort = 1;
+        int oInt = 1;
+        long oLong = 1;
+        float oFloat = 1;
+        double oDouble = 1;
+        char oChar = 1;
+
+        System.out.println(oByte + oShort);
+        System.out.println(oInt + oLong);
+        System.out.println(oFloat + oDouble);
+        System.out.println(oInt + oDouble);
+        System.out.println(oFloat + oLong);
+        System.out.println(oChar + oInt);
+        System.out.println(oFloat + oDouble);
+
+    }
+}
