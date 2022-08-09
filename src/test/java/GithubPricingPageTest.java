3a99b15d1693f2b092d7ac95af220f1538e4eeb3
Pavel Ushakov <ushakovpb@gmail.com>
2022-08-09 12:00:41 +0400

    Добавлен тест страницы с ценами

---
+++ src/test/java/GithubPricingPageTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubPricingPageTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1020";
+    }
+
+    @Test
+    void shouldOpenPricingPage() {
+        open("https://github.com/");
+
+        $(byText("Pricing")).hover();
+        $(byXpath("//nav/ul/li[6]/details/div")).shouldBe(visible);
+
+        $(byText("Compare plans")).click();
+        $("body").shouldHave(text("Choose the plan that’s right for you."));
+    }
+}
