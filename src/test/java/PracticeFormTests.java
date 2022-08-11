40bcfcd9cb69d7ab796ccd81074a04314f07272f
Pavel Ushakov <ushakovpb@gmail.com>
2022-08-11 21:48:18 +0400

    Рефактор - читаемые селекторы

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -25,14 +25,13 @@ public class PracticeFormTests {
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("test@test.com");
         $("#userNumber").setValue("89128560501");
-        $("label[for=\"gender-radio-1\"]").click();
+        $("#genterWrapper").$(byText("Other")).click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionContainingText("September");
         $(".react-datepicker__year-select").selectOptionContainingText("1939");
         $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
-        $("label[for=\"hobbies-checkbox-1\"]").click();
-        $("label[for=\"hobbies-checkbox-3\"]").click();
-        $("#subjectsInput").setValue("Test");
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#subjectsInput").setValue("Math").pressEnter();
         $("#uploadPicture").uploadFile(new File("src/test/resources/File.png"));
         $("#currentAddress").setValue("ул. Пушкина");
         $(byText("Select State")).click();
@@ -45,12 +44,11 @@ public class PracticeFormTests {
         $$("tbody tr").shouldHave(size(10));
         $("tbody tr", 0).shouldHave(text("Student Name"), text("Ivan Ivanov"));
         $("tbody tr", 1).shouldHave(text("Student Email"), text("test@test.com"));
-        $("tbody tr", 2).shouldHave(text("Gender"), text("Male"));
+        $("tbody tr", 2).shouldHave(text("Gender"), text("Other"));
         $("tbody tr", 3).shouldHave(text("Mobile"), text("8912856050"));
         $("tbody tr", 4).shouldHave(text("Date of Birth"), text("01 September,1939"));
-
-        //$("tbody tr", 5).shouldHave(text("Subject"), text("Test"));
-        $("tbody tr", 6).shouldHave(text("Hobbies"), text("Sports, Music"));
+        $("tbody tr", 5).shouldHave(text("Subject"), text("Maths"));
+        $("tbody tr", 6).shouldHave(text("Hobbies"), text("Sports"));
         $("tbody tr", 7).shouldHave(text("Picture"), text("File.png"));
         $("tbody tr", 8).shouldHave(text("Address"), text("ул. Пушкина"));
         $("tbody tr", 9).shouldHave(text("State and City"), text("NCR Noida"));
