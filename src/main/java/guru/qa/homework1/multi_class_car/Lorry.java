17fc9313c2b1292fd1ea742c398c852b6c906713
Pavel Ushakov <ushakovpb@gmail.com>
2022-08-20 10:46:32 +0400

    Зачстка проекта

--- src/main/java/guru/qa/homework1/multi_class_car/Lorry.java
+++
@@ -1,9 +0,0 @@
-package guru.qa.homework1.multi_class_car;
-
-public class Lorry extends Car {
-    int carrying;
-
-    public Lorry(String carClass, Engine engine, Driver driver, String brand) {
-        super(carClass, engine, driver, brand);
-    }
-}
