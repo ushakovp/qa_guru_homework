3a9411319d899c33381e6bfbe9756bdb1c97ae94
Pavel Ushakov <ushakovpb@gmail.com>
2022-08-25 09:38:50 +0400

    Добавлен Selenide + Allure Listener тест

--- src/test/java/com/github/WebTests.java
+++ src/test/java/com/github/WebTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class WebTests {
 
@@ -17,7 +18,7 @@ public class WebTests {
     }
 
     @Test
-    void repositoryHasIssue() {
+    void repositoryHasIssueListenerTest() {
         open("https://github.com/");
         $("[data-test-selector=nav-search-input]").click();
         $("[data-test-selector=nav-search-input]").setValue("selenide");
@@ -26,5 +27,30 @@ public class WebTests {
         $("#issues-tab").click();
         $(byText("Add caching API into standard API")).should(exist);
 
+    }
+
+    @Test
+    void repositoryHasIssueLambdaTest() {
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+
+        step("Ищем репозиторий Selenide", () -> {
+            $("[data-test-selector=nav-search-input]").click();
+            $("[data-test-selector=nav-search-input]").setValue("selenide");
+            $("[data-test-selector=nav-search-input]").submit();
+        });
+
+        step("Переходим в репозиторий Selenide", () -> {
+            $$(".repo-list-item").first().$("a").click();
+        });
+
+        step("Переходим на таб Issue", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("Проверяем, что есть нужная Issue", () -> {
+            $(byText("Add caching API into standard API")).should(exist);
+        });
     }
 }
