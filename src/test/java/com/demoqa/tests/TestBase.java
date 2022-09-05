8a8c726c0fa98297d8daf4bd47f48f6b44564447
Pavel Ushakov <ushakovpb@gmail.com>
2022-09-05 22:03:32 +0400

    Подготовка тестов для запуска с параметрами

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Objects;
+
+import static java.lang.String.format;
+
 public class TestBase {
 
     @BeforeAll
@@ -15,15 +19,17 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("browserName", System.getProperty("browserName", "chrome"));
+        capabilities.setCapability("browserVersion", System.getProperty("browserVersion", "100"));
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        if (Objects.equals(System.getProperty("remote"), "autotest.cloud"))
+            Configuration.remote = format("https://%s:%s@selenoid.autotests.cloud/wd/hub", System.getProperty("remoteUser"), System.getProperty("remoteUserPassword"));
     }
 
     @AfterEach
